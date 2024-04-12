b7b8b835166923e4d1ab3edae1548d711f768f58
sotatek <sotatek@quang.tran>
2024-04-12 14:48:24 +0700

    test jenkinsfile

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     allowedHeaders: '*',
   });
   await app.listen(PORT, () => {
-    console.log('Application is running on the port:', PORT);
+    console.log('Application is running on this port:', PORT);
   });
 }
 bootstrap();
