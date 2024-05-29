25ba286e72a3cee9c7f85d60c97cab14fc0b94c7
sotatek <sotatek@quang.tran>
2024-05-29 16:02:30 +0700

    feat:microservice-gateway

--- src/modules/favorites/controllers/favorite.controller.ts
+++ src/modules/favorites/controllers/favorite.controller.ts
@@ -1,17 +1,31 @@
 import { ControllerCustom } from 'src/decorators';
 import { FavoriteService } from '../services/favorite.sevice';
-import { Body, Get, Post, Query, Request } from '@nestjs/common';
+import {
+  Body,
+  Get,
+  Inject,
+  OnModuleInit,
+  Post,
+  Query,
+  Request,
+} from '@nestjs/common';
 import { FavoriteCreateDto, FavoriteReqDto } from '../dtos/favorite.dto';
 import { PaginateService } from 'src/shared/services/paginate.service';
 import { CriteriaService } from 'src/shared/services/criteria.service';
+import { ClientKafka } from '@nestjs/microservices';
 
 @ControllerCustom('/favorite', 'Favorite', false)
-export class FavoriteController {
+export class FavoriteController implements OnModuleInit {
   constructor(
     private favoriteService: FavoriteService,
     private pageService: PaginateService,
     private criteriaService: CriteriaService,
+    @Inject('FAVORITE') private client: ClientKafka,
   ) {}
+
+  onModuleInit() {
+    this.client.subscribeToResponseOf('productFavourite');
+  }
 
   @Post('/create')
   async addFavorite(@Body() body: FavoriteCreateDto) {
@@ -23,11 +37,19 @@ export class FavoriteController {
     @Query() query: FavoriteReqDto,
     @Request() req: { user: { id: number } },
   ) {
-    query.userId = req.user.id;
+    query.userId = req?.user?.id;
     const { items, total } = await this.favoriteService.findAndCount(
       this.criteriaService.handleParam(query),
       this.pageService.paginate(query),
     );
     return { items, total };
+  }
+
+  @Post('/kafka/send')
+  sendKafkaFavor() {
+    this.client.send('productFavourite', 'test message').subscribe((data) => {
+      console.log('object', data);
+    });
+    return;
   }
 }
--- src/modules/favorites/favorite.module.ts
+++ src/modules/favorites/favorite.module.ts
@@ -3,9 +3,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoriteEntity } from './entities/favorite.entity';
 import { FavoriteService } from './services/favorite.sevice';
 import { FavoriteController } from './controllers/favorite.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FavoriteEntity])],
+  imports: [
+    TypeOrmModule.forFeature([FavoriteEntity]),
+    ClientsModule.register([
+      {
+        name: 'FAVORITE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'favour',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'favour-consumer',
+          },
+        },
+      },
+    ]),
+  ],
   providers: [FavoriteService],
   controllers: [FavoriteController],
 })
